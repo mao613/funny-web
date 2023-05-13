8e6d81e1e1e5ca36bf14d0f2d2759ae8afaa029b
mao613 <mario.antonio.ortiz.torres@gmail.com>
2023-05-13 17:43:19 +0400

    Added the jokes

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+jokes = [
+    "What can you do if you cannot push your git changes? Use the --force, Luke",
+    "Which body part does a programmer know best? ARM",
+    "Relationship status? I'll leave the relations to the database.",
+    "How do you get the code for the bank vault? You checkout their branch.",
+    "How did the developer announce their engagement? They returned true!"]
+
 @app.get("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def tell_a_joke():
+    joke = random.choice(jokes)
+    return f"<p>{joke}</p>"
 
