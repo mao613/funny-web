c4c495589f0809122a315afd01dafe2c3314d933
mao613 <mario.antonio.ortiz.torres@gmail.com>
2023-05-13 22:28:56 +0400

    Finished new data layer refactoring

---
+++ data.py
@@ -0,0 +1,6 @@
+jokes = [
+    "What can you do if you cannot push your git changes? Use the --force, Luke",
+    "Which body part does a programmer know best? ARM",
+    "Relationship status? I'll leave the relations to the database.",
+    "How do you get the code for the bank vault? You checkout their branch.",
+    "How did the developer announce their engagement? They returned true!"]
