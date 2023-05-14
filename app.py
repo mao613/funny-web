89661332a2fc14fc1a1fffa1b22378a087c3b38c
Mario <mario.antonio.ortiz.torres@gmail.com>
2023-05-14 15:39:06 +0400

    Now with logging

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import flask
 from flask import Flask
-import data
+from data import jokes
 import random
 
 app = Flask(__name__)
 
 @app.get("/")
 def tell_a_joke():
-    joke = random.choice(data.jokes)
+    print("Begging to tell a joke!")
+    joke = random.choice(jokes)
+    print(f"The joke we are telling is {joke}")
     return flask.render_template('joke.html', joke_text=joke)
 
